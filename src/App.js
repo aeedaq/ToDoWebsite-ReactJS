688bf1ba6d067fdd21a06a90a9ad3023bc199957
bizhanov <bizhanov@sheridancollege.ca>
2022-03-01 02:49:49 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { func } from 'prop-types';
+import React from 'react';
+import Context from './context';
+import AddTodo from './Todo/AddTodo';
+
+import TodoList from './Todo/TodoList';
 
 function App() {
+
+  let [todos, setTodos] = React.useState([
+    {
+      id: 1,
+      completed: false,
+      title: 'Buy bread'
+    },
+    {
+      id: 2,
+      completed: false,
+      title: 'Buy milk'
+    },
+    {
+      id: 3,
+      completed: false,
+      title: 'Buy butter'
+    }
+  ]
+  )
+  
+
+  function toggleTodo(id) {
+    setTodos(
+    todos = todos.map
+    (todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo
+    }
+    )
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title){
+    setTodos(todos.concat
+      (
+        [
+          {
+            title,
+            id: Date.now(),
+            completed: false
+          }
+        ]
+      )
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Context.Provider value = {{removeTodo}}>
+      <div className="wrapper">
+        <h1>Todo List with REACT JS</h1>
+        <AddTodo
+         onCreate = {addTodo}
+          />
+        {todos.length ? <TodoList 
+          todos={todos} 
+          onToggle={toggleTodo}
+          /> : <p>no todos </p>
+            }
+        
+      </div>
+    </Context.Provider>
+  )
 }
 
 export default App;
---
+++ src/Todo/TodoItem.js
@@ -0,0 +1,60 @@
+import React, {useContext} from "react";
+import PropTypes from 'prop-types'
+import { getSuggestedQuery } from "@testing-library/dom";
+import Context from "../context";
+
+
+const styles = {
+  li: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '.5rem 1rem',
+    border: '1px solid grey',
+    borderRadius: '5px',
+    marginBottom: '3rem'
+  },
+
+  input: {
+    marginRight: '1rem'
+  }
+}
+
+function TodoItem({ todo, index, onChange }) {
+
+  const {removeTodo} = useContext(Context)
+  console.log('todo : ', todo)
+
+  const classes = []
+
+  if(todo.completed) {
+    classes.push('done')
+  }
+  return (
+    
+    <li style = {styles.li}>
+      <span className={classes.join(' ')}>
+        <input 
+          type = "checkbox" 
+          style = {styles.input} 
+          onChange = {() => onChange(todo.id)} 
+          checked = {todo.completed}
+        />
+
+        <strong>{index + 1}</strong>
+        &nbsp;
+        {todo.title}
+      </span>
+      <button className="rm" onClick= {() => removeTodo(todo.id)}>&times;</button>
+      
+    </li>
+  )
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index:  PropTypes.number,
+  onChange: PropTypes.func.isRequired
+}
+
+export default TodoItem;
